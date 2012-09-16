9234bdbe840b4eaeff3878f3666b567f04cd83b9
Joonas Aalto <joaalto@iki.fi>
2012-09-16 22:52:57 +0300

    Added simple search functionality.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,12 +5,13 @@ import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.format.Formats._
 import models.Product
 
 object Application extends Controller {
 
   def products = Action {
-    Ok(views.html.product(Product.all(), productForm))
+    Ok(views.html.product(Product.all(), productForm, filteredProducts, productSearchForm))
   }
 
   def addProduct = Action { implicit request =>
@@ -25,16 +26,26 @@ object Application extends Controller {
       })
   }
 
-  val productForm = Form(
-    mapping(
-      "Id" -> number,
-      "Name" -> text,
-      "Colour" -> text)(Product.apply)(Product.unapply))
+  val productForm = Form(getProductFieldMapping)
 
-  //  val productSearchForm = Form(fields)
-  //
-  //  val fields = tuple(
-  //    "Id" -> number,
-  //    "Name" -> text,
-  //    "Colour" -> text)
+  def getProductFieldMapping = mapping(
+    "Id" -> number,
+    "Name" -> text,
+    "Colour" -> text)(Product.apply)(Product.unapply)
+
+  var filteredProducts: List[Product] = List()
+
+  def searchProducts = Action { implicit request =>
+    productForm.bindFromRequest().fold(
+      errors =>
+        BadRequest(views.html.errors(errors.toString)),
+      product => {
+        filteredProducts = Product.query(product)
+        Logger.debug("Filtered: " + filteredProducts)
+        Redirect(routes.Application.products)
+      })
+  }
+
+  val productSearchForm = Form(getProductFieldMapping)
+
 }
--- app/models/Product.scala
+++ app/models/Product.scala
@@ -1,22 +1,21 @@
 package models
 
 import org.scalaquery.ql.basic.{ BasicTable => Table }
-//import org.scalaquery.ql.basic.BasicDriver.Implicit._
 import org.scalaquery.ql.extended.H2Driver.Implicit._
-
 import org.scalaquery.ql.TypeMapper._
 import org.scalaquery.ql._
 import org.scalaquery.session.Database
 import play.api.db._
 import play.api.Play.current
 import org.scalaquery.session.Session
+import play.api.Logger
 
 case class Product(id: Int, name: String, colour: String)
 
 object Product extends Table[Product]("PRODUCT") {
   lazy val database = Database.forDataSource(DB.getDataSource())
 
-  def id = column[Int]("ID", O.PrimaryKey) // O.AutoInc
+  def id = column[Int]("ID") // O.AutoInc
   def name = column[String]("NAME", O.NotNull, O.DBType("varchar(64)"))
   def colour = column[String]("COLOUR", O.DBType("varchar(64)"))
 
@@ -29,7 +28,11 @@ object Product extends Table[Product]("PRODUCT") {
   }
 
   def all() = database.withSession { implicit session: Session =>
-    (for (prod <- Product) yield prod).list
+    (for (product <- Product) yield product).list
   }
 
+  def query(params: Product) = database.withSession { implicit session: Session =>
+    Logger.debug("Params: " + params)
+    (for (product <- Product if product.name is params.name) yield product).list
+  }
 }
